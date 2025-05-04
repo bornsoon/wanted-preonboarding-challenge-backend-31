effc0ab8bcbfd1d33540260b22cd6fc17cd98128
bornsoon <hyolim922@hotmail.com>
2025-05-04 20:23:35 +0900

    초기 세팅

---
+++ pre-assignment/src/test/java/org/example/preassignment/PreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.preassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
